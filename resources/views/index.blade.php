ad2708dc87276300d21a7a38aec23cd8e2820ea1
mihohohoho <nobudaigo2000@gmail.com>
2021-10-19 03:38:32 +0900

    ページネイト付きブログ一覧作りました

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,23 +2,22 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-
-        <title>Blog</title>
-
+        <title> Blog</title>
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
-        
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
         <h1>Blog Name</h1>
         <div class='posts'>
-        @foreach($posts as $post)
-            <div class='post'>
-                <h2 class='title'>{{$post->title}}</h2>
-                <p class='body'>{{$post->body}}</p>
-            </div>
-        @endforeach
+            @foreach ($posts as $post)
+                <div class='post'>
+                    <h2 class='title'>{{ $post->title }}</h2>
+                    <p class='body'>{{ $post->body }}</p>
+                </div>
+            @endforeach
+        </div>
+        <div class='paginate'>
+            {{ $posts->links() }}
         </div>
     </body>
 </html>
